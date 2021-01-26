eed746c46061fb65b02e873ce0864d1b84e90055
Christopher-Lee Contout <contout.christopher@hotmail.fr>
2021-01-26 10:20:31 +0100

    finition projet

--- js/ajax.js
+++ js/ajax.js
@@ -1,11 +1,18 @@
+const entrypointApi = 'http://localhost:3000/api/';//// Variable globale de l'url
+
+////////////  Récupération des informations de l'API //////////
 
 function get (url){
-    const promise = new Promise (function (resolve){
+    const promise = new Promise(function(resolve, reject){
         let request = new XMLHttpRequest ();
-        request.open("GET", url);
+        request.open('GET', entrypointApi + url);
         request.onreadystatechange = function(){
             if (this.readyState == XMLHttpRequest.DONE && this.status == 200){
                 resolve(JSON.parse(this.responseText));    
+            }else{
+                if (this.readyState == XMLHttpRequest.DONE && this.status != 200){
+                    reject(this.status);
+                }
             } 
         };
         request.send();
@@ -13,22 +20,23 @@ function get (url){
     return promise;
 }
 
-
-
-
  //////////////////////////////////// Envoie des données à l'API//////////////////////////
  
- function post (url,data){
-    const promise = new Promise (function (resolve){
+ function post (url, data){
+    const promise = new Promise(function(resolve, reject){
         let request = new XMLHttpRequest ();
-        request.open("POST", url);
-        request.setRequestHeader("Content-Type", "application/json");
+        request.open('POST', entrypointApi + url);
+        request.setRequestHeader('Content-Type', 'application/json');
         request.onreadystatechange = function(){
-            if (this.readyState == 4 && this.status === 201){
+            if (this.readyState == 4 && this.status == 201){
                 resolve(JSON.parse(this.responseText));   
+            }else{
+                if (this.readyState == 4 && this.status != 201){
+                    reject(this.status);
+                } 
             }
         };
-        request.send (JSON.stringify(data));
+        request.send(JSON.stringify(data));
     });
     return promise; 
 }
---
+++ js/fonctionsPanier.js
@@ -0,0 +1,42 @@
+///////// stockage du panier dans une variable /////////
+
+let  basketProduct = JSON.parse(localStorage.getItem('basketContents')) || []; 
+
+//////////////////////////////// nombre d'articles et produit ajouté au local storage //////////////////
+
+function addToBasket(quantityProduct){  
+    let product = new Product(id, quantityProduct);
+    basketProduct.push(product);
+    localStorage.setItem('basketContents', JSON.stringify(basketProduct));
+} 
+
+///////////////// Affichage état panier : panier vide ou tableau articles /////
+
+function numberArticleNav(){
+    let quantityBasket = document.getElementById('quantityBasket');
+    if(basketProduct == 0 ){
+        quantityBasket.innerHTML = '0';
+        document.getElementById('emptyBasket').style.display = 'block';
+        console.log('le panier est vide');
+    }else{
+        let numberArticle = 0;
+        document.getElementById('infoBasket').style.display = 'block';
+        JSON.parse(localStorage.getItem('basketContents')).forEach((camera)=>{
+            numberArticle += parseInt(camera.quantity);
+            quantityBasket.textContent = numberArticle;
+           
+        });
+        
+        console.log('le panier est plein');
+    }
+}
+numberArticleNav();
+
+///////// Suppression d'un article //////////
+
+function deleteArticle(i){
+    basketProduct.splice(i, 1); //suppression de l'element i du tableau   
+    localStorage.clear(); //on vide le storage avant de le mettre à jour
+    localStorage.setItem('basketContents', JSON.stringify(basketProduct)); //mise à jour du panier sans l'élément i
+    window.location.reload(); //////// Actualisation de la page  
+}   
